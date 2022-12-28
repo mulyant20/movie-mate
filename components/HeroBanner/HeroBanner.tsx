740222d1c3b57cfc60613aea7a6491362df974d1
mulyant20 <mulyan.t20@gmail.com>
2022-12-29 02:39:29 +0700

    feat: add swr and framer

--- components/HeroBanner/HeroBanner.tsx
+++ components/HeroBanner/HeroBanner.tsx
@@ -1,9 +1,11 @@
-import Image from 'next/image'
 import React, { useState, useEffect, use } from 'react'
 import { MovieI } from '../../interfaces/movies'
-import { Layout } from '@components/Layout'
+import useSWR from 'swr'
+import Link from 'next/link'
+import { motion } from 'framer-motion'
 
 const baseUrl = 'https://image.tmdb.org/t/p/original'
+const fetcher = (url: string) => fetch(url).then((res) => res.json())
 
 type Props = {
   request: string
@@ -12,31 +14,42 @@ type Props = {
 export default function HeroBanner({ request }: Props) {
   const [movies, setMovies] = useState<MovieI[] | null>(null)
   const [index, setIndex] = useState<number>(0)
-
-  const getMovies = async () => {
-    const res = await fetch(request)
-    const data = await res.json()
-    setMovies(data.results)
-  }
+  const { data, error } = useSWR(request, fetcher)
 
   useEffect(() => {
-    getMovies()
-  }, [])
+    if (data) {
+      setMovies(data.results)
+    }
+  }, [data])
 
   useEffect(() => {
     const interval = setInterval(() => {
       setIndex((prev) => (prev + 1) % 20)
-    }, 5000)
+    }, 10000)
 
     return () => clearInterval(interval)
   }, [])
 
   if (!Array.isArray(movies) || !movies.length) {
-    return <div className={style.base}></div>
+    return (
+      <motion.div
+        initial={{ opacity: 0, scale: 0 }}
+        animate={{ opacity: 1, scale: 1 }}
+        exit={{ scale: 0, opacity: 0 }}
+        transition={{ duration: 0.2, ease: 'easeOut' }}
+        className={style.base}
+      ></motion.div>
+    )
   }
 
   return (
-    <div className={style.base}>
+    <motion.div
+      initial={{ opacity: 0, scale: 0 }}
+      animate={{ opacity: 1, scale: 1 }}
+      exit={{ scale: 0, opacity: 0 }}
+      transition={{ duration: 0.2, ease: 'easeOut' }}
+      className={style.base}
+    >
       <div className={style.layer}>
         <div className={style.layerInner}>
           <div className={style.label}>
@@ -44,12 +57,12 @@ export default function HeroBanner({ request }: Props) {
             <p className={style.labelNumber}>{movies[index].vote_average}</p>
           </div>
           {movies[index].media_type === 'movie' ? (
-            <h1 className={style.title}>
+            <Link className={style.title} href={`/detail/${movies[index].id}`}>
               {movies[index].title}{' '}
               <span className={style.year}>
                 {movies[index].release_date.split('-')[0]}
               </span>
-            </h1>
+            </Link>
           ) : (
             <h1 className={style.title}>
               {movies[index].name}{' '}
@@ -67,18 +80,20 @@ export default function HeroBanner({ request }: Props) {
           className={style.img}
         />
       </div>
-    </div>
+    </motion.div>
   )
 }
 
 const style = {
-  layer: 'w-full h-full absolute left-0 top-0 bg-gradient-to-t from-[#1A171E] to-[#1A171E]/0 rounded-[16px]',
+  layer:
+    'w-full h-full absolute left-0 top-0 bg-gradient-to-t from-[#1A171E] to-[#1A171E]/0 rounded-[16px]',
   layerInner: 'absolute bottom-[40px] left-[40px]',
-  label:'inline-flex gap-2 items-center rounded-full px-[12px] py-[8px] bg-black/70 mb-4',
+  label:
+    'inline-flex gap-2 items-center rounded-full px-[12px] py-[8px] bg-black/70 mb-4',
   labelNumber: 'text-[14px] text-white',
-  title: 'text-3xl font-semibold text-white',
+  title: 'text-3xl font-semibold text-white block cursor-pointer',
   year: 'text-lg font-light text-white/50',
   imgWrapper: 'w-full h-full rounded-[20px] overflow-hidden',
-  img: 'w-full object-cover object-top rounded-[18px]',
+  img: 'w-full h-full object-cover object-top rounded-[18px]',
   base: 'w-full h-72 bg-[#1A171E] rounded-[18px] relative mb-12',
 }
