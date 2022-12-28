27c21c2c5397a1110d63ac6b7973e52d42250742
mulyant20 <mulyan.t20@gmail.com>
2022-12-29 02:39:13 +0700

    feat: add framer

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Sidebar } from '../Sidebar'
+import { AnimatePresence, motion } from 'framer-motion'
 
 type Props = {
   children: React.ReactNode
@@ -8,12 +9,20 @@ type Props = {
 export default function Layout({ children }: Props) {
   return (
     <div className='w-full min-h-screen'>
-        <Sidebar/>
-        <div className='ml-72 pt-8 max-w-[calc(100vw-18rem)]'>
-          <div className='w-[calc(100%-8rem)] xl:max-w-[1000px] h-20 mx-auto'>
+      <Sidebar />
+      <AnimatePresence>
+        <motion.div
+          initial={{ opacity: 0, y: '10%' }}
+          animate={{ opacity: 1, y: '0' }}
+          exit={{ y: '-60%', opacity: 0.5 }}
+          transition={{ duration: 0.1, ease: 'easeIn' }}
+          className='ml-72 py-8 max-w-[calc(100vw-18rem)] h-fit mb-12'
+        >
+          <div className='w-[calc(100%-8rem)] xl:max-w-[1000px] h-fit mx-auto'>
             {children}
           </div>
-        </div>
+        </motion.div>
+      </AnimatePresence>
     </div>
   )
 }
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,3 @@
-
 import { HeroBanner } from '@components/HeroBanner'
 import { Layout } from '@components/Layout'
 import { RowMovies } from '@components/RowMovies'
@@ -9,16 +8,22 @@ const baseUrl = 'https://api.themoviedb.org/3'
 const requests = {
   featured: `${baseUrl}/trending/all/day?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}&language=en-US`,
   trends: `${baseUrl}/trending/all/day?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}&page=2`,
-  topRated: `${baseUrl}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}`
+  topRated: `${baseUrl}/movie/top_rated?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}`,
+  action: `${baseUrl}/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}&with_genres=28`,
+  comedy: `${baseUrl}/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}&with_genres=35`,
+  romance: `${baseUrl}/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}&with_genres=10749`,
+  documentaries: `${baseUrl}/discover/movie?api_key=${process.env.NEXT_PUBLIC_API_TOKEN}&with_genres=99`,
 }
 
 export default function Home() {
-  
   return (
     <Layout>
-      <HeroBanner request={requests.featured}/>
+      <h1 className='text-xl text-white font-semibold mb-6'>Featured</h1>
+      <HeroBanner request={requests.featured} />
       <RowMovies title='Trending' request={requests.trends} />
-      <RowMovies title='Top Rated' request={requests.topRated}/>
+      <RowMovies title='Top Rated' request={requests.topRated} />
+      <RowMovies title='Action' request={requests.action} />
+      <RowMovies title='Comedy' request={requests.comedy} />
     </Layout>
   )
 }
