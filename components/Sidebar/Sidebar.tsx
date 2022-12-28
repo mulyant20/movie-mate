7fca7f87b6f3a3de88ca0a9d55f2dd6af1173cfc
mulyant20 <mulyan.t20@gmail.com>
2022-12-29 02:40:57 +0700

    style: add bg white on hover and isActive

--- components/Sidebar/Sidebar.tsx
+++ components/Sidebar/Sidebar.tsx
@@ -1,28 +1,33 @@
-import Link from 'next/link'
-import { useRouter } from 'next/router';
+import { useRouter } from 'next/router'
 import React from 'react'
-import SidebarMenu from './SidebarMenu';
+import SidebarMenu from './SidebarMenu'
 
 export default function Sidebar() {
-  
-  const router = useRouter();
-  
+  const router = useRouter()
+
   const isActiveRoute = () => {
     let temp: string[]
-    if(router.asPath.includes('/')) {
+    if (router.asPath.includes('/')) {
       temp = router.asPath.split('/')
       return `/${temp[1]}`
     }
-  };
+  }
 
   return (
     <div className='fixed h-screen w-72 bg-[#1A171E] top-0 left-0'>
+      <p className='mt-8 mb-8 text-lg text-white mx-6'>Movie Mate</p>
       <div>
-        <p className='mx-6 text-sm text-white/30 mb-3 mt-8'>Menu</p>
+        <p className='mx-6 text-sm text-white/30 mb-3'>Menu</p>
         <div className='flex flex-col gap-3'>
-          <SidebarMenu route={isActiveRoute()} href='/'>Browse</SidebarMenu>
-          <SidebarMenu route={isActiveRoute()} href='/category'>Category</SidebarMenu>
-          <SidebarMenu route={isActiveRoute()} href='/watchlist'>Watchlist</SidebarMenu>
+          <SidebarMenu route={isActiveRoute()} href='/'>
+            Browse
+          </SidebarMenu>
+          <SidebarMenu route={isActiveRoute()} href='/category'>
+            Category
+          </SidebarMenu>
+          <SidebarMenu route={isActiveRoute()} href='/watchlist'>
+            Watchlist
+          </SidebarMenu>
         </div>
       </div>
     </div>
--- components/Sidebar/SidebarMenu.tsx
+++ components/Sidebar/SidebarMenu.tsx
@@ -19,11 +19,13 @@ export default function SidebarMenu({ href, route, children }: Props) {
   }, [])
 
   return (
-    <div className={`w-full py-2 px-8 justify-between relative`}>
+    <div className='w-full py-2 px-4 justify-between relative'>
       {isActive ? (
         <div className='h-[80%] w-[4px] absolute top-1/2 -translate-y-1/2 left-0 bg-red-500 rounded-r-lg'></div>
       ) : null}
-      <Link href={href} className={isActive ? 'text-white' : 'text-white/50'}>{children}</Link>
+      <div className={`w-full px-6 py-2 rounded-lg hover:bg-white/10 ${isActive ? 'bg-white/10' : null}`}>
+        <Link href={href} className={isActive ? 'text-white' : 'text-white/50'}>{children}</Link>
+      </div>
     </div>
   )
 }
