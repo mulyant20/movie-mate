e2669a5d9aea315981d90d6d90d1991487197d8e
mulyant20 <mulyan.t20@gmail.com>
2022-12-30 06:49:53 +0700

    update watchlist

--- pages/watchlist.tsx
+++ pages/watchlist.tsx
@@ -2,19 +2,15 @@ import { Layout } from '@components/Layout'
 import { CardWatchlist } from '@components/RowMovies'
 import { watchListI } from '@interfaces/watchlist'
 import React, { useEffect, useState } from 'react'
-import { useSelector, useDispatch } from 'react-redux'
 
 type Props = {}
 
 export default function watchlist({}: Props) {
-  const watchlists = useSelector((state: any) => state.watchlist)
   const [movies, setMovies] = useState<watchListI[] | []>([])
-  const [isLocal, setIsLocal] = useState<boolean>(false)
 
   useEffect(() => {
     const local = localStorage.getItem('watchlist')
     if (local) {
-      setIsLocal(true)
       setMovies(JSON.parse(local))
     }
   }, [])
@@ -23,11 +19,7 @@ export default function watchlist({}: Props) {
     <Layout>
       <h1 className='text-xl text-white font-semibold mb-6'>Watchlist</h1>
       <div className='grid grid-cols-[repeat(auto-fill,minmax(theme(width.64),1fr))] gap-8 mt-8 mb-8 relative h-fit w-full'>
-        {isLocal
-          ? movies.map((movie: watchListI, index: number) => <CardWatchlist key={index} movie={movie} />)
-          : watchlists.map((watchlist: watchListI, index: number) => (
-              <CardWatchlist key={index} movie={watchlist} />
-            ))}
+        {movies.map((movie: watchListI, index: number) => <CardWatchlist key={index} movie={movie} />)}
       </div>
     </Layout>
   )
